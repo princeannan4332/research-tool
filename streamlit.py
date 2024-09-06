b9118b6b7b382db231f60cb9a63468254533c1d6
princeannan4332 <97059029+princeannan4332@users.noreply.github.com>
2024-09-06 15:01:00 +0000

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,16 +1,2 @@
-import os
-import pandas as pd
-import PyPDF2
 import streamlit as st
-import time
-from langchain import OpenAI
-
-
-
-load_dotenv()
-
-os.environ['OPENAI_API_KEY']=os.getenv('api_key')
-os.environ["PINECONE_API_KEY"]= os.getenv('pinecone_api_key')#get from app.pinecone.io
-
-
 st.header("Testing")
