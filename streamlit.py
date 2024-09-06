b4e99cac2e768949391ae8537684589899eac4c6
princeannan4332 <97059029+princeannan4332@users.noreply.github.com>
2024-09-06 14:59:40 +0000

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -4,15 +4,7 @@ import PyPDF2
 import streamlit as st
 import time
 from langchain import OpenAI
-from langchain.chains import RetrievalQAWithSourcesChain
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.document_loaders import UnstructuredURLLoader
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.docstore.document import Document
-from dotenv import load_dotenv
-from langchain_pinecone import PineconeVectorStore
-from langchain.vectorstores import Pinecone
+
 
 
 load_dotenv()
