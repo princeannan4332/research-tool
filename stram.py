89d1e6380c99e2f629349bbbd019c4af4ac5dc37
princeannan4332 <97059029+princeannan4332@users.noreply.github.com>
2024-09-06 01:33:35 +0000

    Add files via upload

---
+++ stram.py
@@ -0,0 +1,17 @@
+import os
+import pandas as pd
+import PyPDF2
+import streamlit as st
+import pickle
+import time
+from langchain import OpenAI
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import UnstructuredURLLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.docstore.document import Document
+from dotenv import load_dotenv
+
+
+st.header("starter")
