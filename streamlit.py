fd715c3dbde512e5badb9771ef29ffed66d3cacc
princeannan4332 <97059029+princeannan4332@users.noreply.github.com>
2024-09-06 19:33:07 +0000

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -141,7 +141,7 @@ if process_url_clicked:
     docs = text_splitter.split_documents(data)
     # create embeddings and save it to FAISS index
     embeddings = OpenAIEmbeddings()
-    vectorstore=PineconeVectorStore.from_documents(docs,embeddings,index_name="pinecone-v1")
+    vectorstore=PineconeVectorStore.from_documents(docs,embeddings,index_name="unicef")
     main_placeholder.text("Embedding Vector Started Building...✅✅✅")
     time.sleep(2)
 
@@ -151,7 +151,7 @@ if process_url_clicked:
 
 
 
-index_name='pinecone-v1'#name of the index we created in pinecone
+index_name='unicef'#name of the index we created in pinecone
 
 query = main_placeholder.text_input("Question: ")
 if query:
